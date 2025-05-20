29f1d0faf0d9a4a2351bcaed33f4200d5a10e5ed
Anonymous-craj <sirajbasnet5@gmail.com>
2025-05-20 21:52:28 +0545

    connected react project with store using redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 function App() {
-  return <h1 className="font-bold text-3xl underline">Hello World!</h1>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>This is home page!</h1>} />
+          <Route path="/register" element={<h1>This is register page!</h1>} />
+          <Route path="/login" element={<h1>This is login page!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
