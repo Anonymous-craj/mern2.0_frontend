abee8ff9805d201942dc2e87ebe760fd2f61d8e9
Anonymous-craj <sirajbasnet5@gmail.com>
2025-05-22 16:49:23 +0545

    created asyncThunk for login and register

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import type { AppDispatch } from "./store";
+import API from "../http";
 
 interface User {
   username: string;
@@ -8,14 +10,31 @@ interface User {
   token: string;
 }
 
+interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+export enum StatusData {
+  Loading = "loading",
+  Success = "success",
+  Error = "error",
+}
+
 interface AuthState {
   user: User;
-  status: string;
+  status: StatusData;
 }
 
 const initialState: AuthState = {
   user: {} as User,
-  status: "pending",
+  status: StatusData.Loading,
 };
 
 const authSlice = createSlice({
@@ -26,7 +45,7 @@ const authSlice = createSlice({
       state.user = action.payload;
     },
 
-    setStatus(state: AuthState, action: PayloadAction<string>) {
+    setStatus(state: AuthState, action: PayloadAction<StatusData>) {
       state.status = action.payload;
     },
   },
@@ -34,3 +53,37 @@ const authSlice = createSlice({
 
 export const { setUser, setStatus } = authSlice.actions;
 export default authSlice.reducer;
+
+export function register(data: RegisterData) {
+  return async function registerThunk(dispatch: AppDispatch) {
+    dispatch(setStatus(StatusData.Loading));
+    try {
+      const response = await API.post("register", data);
+      if (response.status === 201) {
+        dispatch(setStatus(StatusData.Success));
+      } else {
+        dispatch(setStatus(StatusData.Error));
+      }
+    } catch (error) {
+      console.error("Register failed:", error);
+      dispatch(setStatus(StatusData.Error));
+    }
+  };
+}
+
+export function login(data: LoginData) {
+  return async function loginThunk(dispatch: AppDispatch) {
+    dispatch(setStatus(StatusData.Loading));
+    try {
+      const response = await API.post("login", data);
+      if (response.status === 200) {
+        dispatch(setStatus(StatusData.Success));
+      } else {
+        dispatch(setStatus(StatusData.Error));
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+      dispatch(setStatus(StatusData.Error));
+    }
+  };
+}
